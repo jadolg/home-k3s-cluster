219ce02a6d4772e6fc01f779c27e350b37eb5f79
Jorge Alberto Díaz Orozco <jorge.diaz@riskident.com>
2023-11-12 16:07:47 +0100

    Mesh namespaces for argo applications

--- k8s/modules/applications/main.tf
+++ k8s/modules/applications/main.tf
@@ -1,15 +1,31 @@
 terraform {
   required_providers {
     kubectl = {
-      source  = "gavinbunney/kubectl"
+      source = "gavinbunney/kubectl"
+    }
+  }
+}
+
+locals {
+  namespaces = toset(["klum", "shadowtest"])
+}
+
+resource "kubernetes_namespace" "namespaces" {
+  for_each = local.namespaces
+  metadata {
+    name = each.value
+    annotations = {
+      "linkerd.io/inject" = "enabled"
     }
   }
 }
 
 resource "kubectl_manifest" "klum" {
-  yaml_body = file("modules/applications/klum.yaml")
+  depends_on = [kubernetes_namespace.namespaces]
+  yaml_body  = file("modules/applications/klum.yaml")
 }
 
 resource "kubectl_manifest" "shadowtest" {
-  yaml_body = file("modules/applications/shadowtest.yaml")
+  depends_on = [kubernetes_namespace.namespaces]
+  yaml_body  = file("modules/applications/shadowtest.yaml")
 }
